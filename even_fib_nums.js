05a4b10297859100a301f9cb605ee6530b1844b7
liznomura <lizn@hawaii.edu>
2017-06-16 10:03:05 -1000

    first two tests pass

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 /**
  * Return the total sum of all even fibonacci numbers up to and including
  * the value given to you at `maxFibValue`
@@ -5,13 +6,27 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
+
+ function isFib(n) {
+  let plusFour = 5 * Math.pow(n, 2) + 4;
+  let minusFour = 5 * Math.pow(n, 2) - 4;
+  if (Number.isInteger(Math.sqrt(plusFour)) === true || Number.isInteger(Math.sqrt(minusFour)) === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function _sumFibs( maxFibValue ) {
   var sum = 0;
-
-  // do your work here
-
+  for (var i = 0; i <= maxFibValue; i++) {
+    if (i % 2 === 0 && isFib(i) === true) {
+      sum += i;
+    }
+  }
   return sum;
 }
+
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
@@ -29,7 +44,7 @@ function _highestFibonacciNumber (maxFibValue){
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
-module.exports = {
+ module.exports = {
   sumFibs : _sumFibs,
   highestFibonacciNumber : _highestFibonacciNumber
 };
